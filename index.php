2d580c76a6539bfaad7e78071f905416b8c66153
Ajay Prakash <43914353+ajayprakash77@users.noreply.github.com>
2020-07-04 16:07:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,61 +17,11 @@
         </div>
     </div>
 
-    <div class="n-content" >
-      <div class="n-categories" >
-        Categories
-        <ul>
-          <li class="n-category n-padding n-blue-hover">Televisions</li>
-          <li class="n-category n-padding n-blue-hover">Smartphones</li>
-        </ul>
-      </div>
-      <div class="n-logoimage">  THIS IS THE NEW CODE
-         <img src="https://pbs.twimg.com/profile_images/852143274504716289/2moYOBhZ.jpg" alt="" width="300" height="300">
-      </div>
-    
-    </div>
-
-
-
-<!-- Bottom Bar -->
-    <div class="n-selected-bar n-blue n-inline n-center">
-        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
-        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
-        <a class="n-button n-rounded n-blue-hover n-right">CheckOut Items</a>
-    </div>
-</body>
-</html>
-<html>
-    <head>
-        <title>Nile - 24/7</title>
-        <link rel="stylesheet" href="css/nile-min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-<body class="n-nomargin"  >
-  <!-- Nav Bar -->
-    <div class="n-navbar n-blue ">
-        <div class="n-left n-button n-blue-hover n-rounded"><i class="fa fa-shopping-basket n-padding"></i>  Nile-Shopping</div>
-        <div class=" n-right">
-            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-home"></i>Home</button>
-            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-reorder"></i>Categories</button>
-            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-cart-plus"></i> Cart</button>
-            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Login</button>
-        </div>
-    </div>
-
-    <div class="n-content" >
-      <div class="n-categories" >
-        Categories
-        <ul>
-          <li class="n-category n-padding n-blue-hover">Televisions</li>
-          <li class="n-category n-padding n-blue-hover">Smartphones</li>
-        </ul>
-      </div>
+    <div>   
       <div class="n-logoimage">
          <img src="https://pbs.twimg.com/profile_images/852143274504716289/2moYOBhZ.jpg" alt="" width="300" height="300">
       </div>
-    
+
     </div>
 
 
