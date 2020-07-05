0cb749ffa58b18bd7e7ecd27364b59b2e2ca60e5
Ajay Prakash <43914353+ajayprakash77@users.noreply.github.com>
2020-07-06 00:09:46 +0530

    Add files via upload

---
+++ television/index.php
@@ -0,0 +1,77 @@
+<html>
+    <head>
+        <title>Nile - 24/7</title>
+        <link rel="stylesheet" href="../css/nile-min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+<body class="n-nomargin"  >
+  <!-- Nav Bar -->
+    <div class="n-navbar n-blue ">
+        <div class="n-left n-button n-blue-hover n-rounded"><i class="fa fa-shopping-basket n-padding"></i>  Nile-Shopping</div>
+        <div class=" n-right">
+            <a href="../index.php">
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-home"></i>Home</button>
+            </a>
+            <a href="../shop/index.php">
+             <button class="n-button n-rounded n-blue-hover"><i class="fa fa-reorder"></i>Categories
+             </button>
+            </a>
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-cart-plus"></i> Cart</button>
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Login</button>
+        </div>
+    </div>
+
+    <div class="n-content" >
+      <div class="n-categories" >
+        Categories
+        <ul>
+          <li class="n-category n-padding n-blue-hover">Televisions</li>
+          <li class="n-category n-padding n-blue-hover">Smartphones</li>
+        </ul>
+      </div>
+      <div class="n-items">
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/k9u8zgw0/television/d/t/k/mi-l32m5-al-original-imafrjtbngu9hrwz.jpeg?q=70" alt="">
+          <name>Mi 4A PRO 80 cm (32) HD Ready LED</name>
+          <info>Smart Android TV With Google Data Saver</info>
+          <price>Rs 29000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/kae95e80/television/f/6/y/realme-tv-32-original-imafrz79pweqeafh.jpeg?q=70" alt="">
+          <name>Realme 80cm (32 inch) HD Ready LED</name>
+          <info>Smart Android TV  (TV 32) </info>
+          <price>Rs 12000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k7nnrm80/television/g/g/g/samsung-ua32t4010arxxl-ua32t4010arxxl-original-imafpuh8fdy746bj.jpeg?q=70" alt="">
+          <name>Samsung 80cm (32 inch)</name>
+          <info>HD Ready LED TV  </info>
+          <price>Rs 12000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/k83dlzk0/television/6/v/h/lg-43um7290ptf-original-imafq6yp8fagh25s.jpeg?q=70" alt="">
+          <name>Nokia 108cm (43 inch)</name>
+          <info>Ultra HD (4K) LED Smart Android TV with Sound by JBL </info>
+          <price>Rs 30000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/kazor680/television/c/x/h/nokia-43cauhdn-original-imafsfgq8dzpcuch.jpeg?q=70" alt="">
+          <name>LG 108cm (43 inch) Ultra HD (4K) </name>
+          <info>LED Smart TV </info>
+          <price>Rs 31000</price>
+        </div>
+      </div>
+
+    </div>
+
+
+
+<!-- Bottom Bar -->
+    <div class="n-selected-bar n-blue n-inline n-center">
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <a class="n-button n-rounded n-blue-hover n-right">CheckOut Items</a>
+    </div>
+</body>
+</html>
