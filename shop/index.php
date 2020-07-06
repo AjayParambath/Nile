07e55323acb1ac3a35b2c3905bb784652f1df4d1
Ajay Prakash <43914353+ajayprakash77@users.noreply.github.com>
2020-07-06 10:52:58 +0530

    Update index.php

--- shop/index.php
+++ shop/index.php
@@ -10,10 +10,10 @@
     <div class="n-navbar n-blue ">
         <div class="n-left n-button n-blue-hover n-rounded"><i class="fa fa-shopping-basket n-padding"></i>  Nile-Shopping</div>
         <div class=" n-right">
-            <a href="index.php">
+            <a href="../index.php">
             <button class="n-button n-rounded n-blue-hover"><i class="fa fa-home"></i>Home</button>
             </a>
-            <a href="shop/index.php">
+            <a href="../shop/index.php">
              <button class="n-button n-rounded n-blue-hover"><i class="fa fa-reorder"></i>Categories
              </button>
             </a>
@@ -30,7 +30,7 @@
           <li class="n-category n-padding n-blue-hover">Smartphones</li>
         </ul>
       </div>
-      <div class="n-items">
+      <div id="Smartphones" class="n-items">
         <div class="n-item">
           <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt="">
           <name>Samsung Note 10</name>
@@ -59,6 +59,38 @@
           <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt="">
           <name>Samsung Note 10</name>
           <info>8GB RAM | 128 GB </info>
+          <price>Rs 31000</price>
+        </div>
+      </div>
+      <div id="TV" class="n-items">
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/k9u8zgw0/television/d/t/k/mi-l32m5-al-original-imafrjtbngu9hrwz.jpeg?q=70" alt="">
+          <name>Mi 4A PRO 80 cm (32) HD Ready LED</name>
+          <info>Smart Android TV With Google Data Saver</info>
+          <price>Rs 29000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/kae95e80/television/f/6/y/realme-tv-32-original-imafrz79pweqeafh.jpeg?q=70" alt="">
+          <name>Realme 80cm (32 inch) HD Ready LED</name>
+          <info>Smart Android TV  (TV 32) </info>
+          <price>Rs 12000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k7nnrm80/television/g/g/g/samsung-ua32t4010arxxl-ua32t4010arxxl-original-imafpuh8fdy746bj.jpeg?q=70" alt="">
+          <name>Samsung 80cm (32 inch)</name>
+          <info>HD Ready LED TV  </info>
+          <price>Rs 12000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/k83dlzk0/television/6/v/h/lg-43um7290ptf-original-imafq6yp8fagh25s.jpeg?q=70" alt="">
+          <name>Nokia 108cm (43 inch)</name>
+          <info>Ultra HD (4K) LED Smart Android TV with Sound by JBL </info>
+          <price>Rs 30000</price>
+        </div>
+        <div class="n-item">
+          <img class="n-image" src="https://rukminim1.flixcart.com/image/312/312/kazor680/television/c/x/h/nokia-43cauhdn-original-imafsfgq8dzpcuch.jpeg?q=70" alt="">
+          <name>LG 108cm (43 inch) Ultra HD (4K) </name>
+          <info>LED Smart TV </info>
           <price>Rs 31000</price>
         </div>
       </div>
