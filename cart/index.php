d2b431e58f1d8b9e6f4f04f6c42fb9a91031c9a6
Ajay Prakash <43914353+ajayprakash77@users.noreply.github.com>
2020-07-06 11:34:05 +0530

    Add files via upload

---
+++ cart/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Nile - 24/7</title>
+        <link rel="stylesheet" href="../css/nile-min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+<body class="n-nomargin"  >
+<!-- Nav Bar -->
+    <div class="n-navbar n-blue ">
+        <div class="n-left n-button n-blue-hover n-rounded"><i class="fa fa-shopping-basket n-padding"></i>  Nile-Shopping</div>
+        <div class=" n-right">
+            <a href="../index.php">
+                <button class="n-button n-rounded n-blue-hover"><i class="fa fa-home"></i>Home</button>
+            </a>
+            <a href="../shop/index.php">
+                <button class="n-button n-rounded n-blue-hover"><i class="fa fa-reorder"></i>Categories
+             </button>
+            </a>
+            <a href="index.php">
+                <button class="n-button n-rounded n-blue-hover"><i class="fa fa-cart-plus"></i> Cart</button>
+            </a>
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Login</button>
+        </div>
+    </div>
+<!-- Main Content -->
+  <div class="n-cart">
+    <p>Selected items</p>
+    <price> Total Amt </price>
+    <br>
+    <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Proceed to checkout</button>
+  </div>
+
+<!-- Bottom Bar -->
+    <div class="n-selected-bar n-blue n-inline n-center">
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <a class="n-button n-rounded n-blue-hover n-right">CheckOut Items</a>
+    </div>
+</body>
+</html>
