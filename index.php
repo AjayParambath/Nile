7a51eae7aba74c351be629933ef3c0ebdaa76320
Adityanpk <61836096+Adityanpk@users.noreply.github.com>
2020-07-06 06:55:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+        <title>Nile - 24/7</title>
+        <link rel="stylesheet" href="css/nile-min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+<body class="n-nomargin" onload ="currentSlide(1)" >
+  <!-- Nav Bar -->
+
+    <div class="n-navbar n-blue ">
+        <div class="n-left n-button n-blue-hover n-rounded"><i class="fa fa-shopping-basket n-padding"></i>  Nile-Shopping</div>
+        <div class=" n-right">
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-home"></i>Home</button>
+            <a href="shop/phones.php">
+             <button class="n-button n-rounded n-blue-hover"><i class="fa fa-reorder"></i>Categories
+             </button>
+            </a>
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-cart-plus"></i> Cart</button>
+            <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Login</button>
+        </div>
+    </div>
+<script src="js/slideimg.js"></script>
+
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <img src="slideimg/img1.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="slideimg/img2.jpg" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="slideimg/img3.jpg" style="width:100%">
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+    <div>   
+	 <div class="n-logoimage">
+         <img src="https://pbs.twimg.com/profile_images/852143274504716289/2moYOBhZ.jpg" alt="" width="150" height="150" >
+      </div>
+    </div> 
+
+    <div class="n-selected-bar n-blue n-inline n-center">
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <div class="n-selected-item "><img class="n-image" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/m/5/p/na-128-a-pafb0023in-motorola-4-original-imafhmzvrhbrzs5c.jpeg?q=70" alt=""></div>
+        <a class="n-button n-rounded n-blue-hover n-right">CheckOut Items</a>
+    </div>
+    <div class="row">
+  		<div class="column">
+    		<div class="card">
+      			<img src="slideimg/img3.jpg" alt="Jane" align="center">
+      			<div class="container">
+        			<h2>item1</h2>
+        			<p class="title">1000</p>
+        			<p><button class="button">Add to cart</button></p>
+      			</div>
+    		</div>
+  		</div>
+
+  		<div class="column">
+    		<div class="card">
+      			<img src="slideimg/img3.jpg" alt="Mike" >
+      			<div class="container">
+        			<h2>item2</h2>
+        			<p class="title">3000</p>
+        			<p><button class="button">Add to cart</button></p>
+      			</div>
+    		</div>
+  		</div>
+  
+  		<div class="column">
+    		<div class="card">
+      			<img src="slideimg/img3.jpg" alt="John" >
+     			<div class="container">
+        			<h2>item3</h2>
+        			<p class="title">25252r</p>
+        			<p><button class="button">Add to cart</button></p>
+      			</div>
+    		</div>
+  		</div>
+	</div>
+</body>
+</html>
